41e62db365bb4a8fece27b0684c3e77be9fca6fb
AdityaPrakash2 <adityaprakash2003.ap@gmail.com>
2024-11-21 10:32:38 -0800

    Initial Commit

---
+++ COMP-49X-24-25-PhoneArt/COMP_49X_24_25_PhoneArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  COMP_49X_24_25_PhoneArtApp.swift
+//  COMP-49X-24-25-PhoneArt
+//
+//  Created by Aditya Prakash on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct COMP_49X_24_25_PhoneArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
